bb82bb61876c273942b681ce96bf6c736c116c9c
Chitwan-bajpai <166581772+Chitwan-bajpai@users.noreply.github.com>
2026-09-14 10:51:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (30.21%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans;
+        int n1=word1.length();
+        int n2=word2.length();
+        int n=max(n1,n2);
+        for(int i=0;i<n;i++){
+            if(i<n1){
+                ans.push_back(word1[i]);
+            }
+            if(i<n2){
+                ans.push_back(word2[i]);
+            }
+        }
+        return ans;
+    }
+};
